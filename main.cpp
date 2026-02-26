0c3cbab4d4071c43381d28cbe930a2d34f069e57
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-02-26 15:54:55 +0100

    ef

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <optional>
 #include <set>
 #include <algorithm>
+#include <fstream>
 
 #define GLFW_INCLUDE_VULKAN
 #define VK_USE_PLATFORM_WIN32_KHR
@@ -15,6 +16,7 @@
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #include <glm/vec4.hpp>
 #include <glm/mat4x4.hpp>
+
 
 #pragma comment(lib, "vulkan-1.lib")
 
@@ -35,6 +37,41 @@ const std::vector<const char *> deviceExtensions = {
         return graphicsFamily.has_value() && presentFamily.has_value();
     }
 };
+
+static std::vector<char> readFile(const std::string& filename) {
+    std::ifstream file(filename, std::ios::ate | std::ios::binary);
+
+    if (!file.is_open()) {
+        throw std::runtime_error("failed to open file!");
+    }
+
+    size_t fileSize = (size_t) file.tellg();
+std::vector<char> buffer(fileSize);
+
+file.seekg(0);
+file.read(buffer.data(), fileSize);
+
+file.close();
+
+return buffer;
+}
+
+void createGraphicsPipeline() {
+    auto vertShaderCode = readFile("shaders/vert.spv");
+    auto fragShaderCode = readFile("shaders/frag.spv");
+}
+
+VkShaderModule createShaderModule(const std::vector<char>& code) {
+VkShaderModuleCreateInfo createInfo{};
+createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+createInfo.codeSize = code.size();
+createInfo.pCode = reinterpret_cast<const uint32_t*>(code.data());
+
+VkShaderModule shaderModule;
+if (vkCreateShaderModule(device, &createInfo, nullptr, &shaderModule) != VK_SUCCESS) {
+    throw std::runtime_error("failed to create shader module!");
+}    
+}
 
 // struct QueueFamilyIndices {
 //     uint32_t graphicsFamily;
@@ -471,6 +508,8 @@ int main()
 
     createSwapChain();
     createImageViews();
+
+    createGraphicsPipeline();
 
     while (!glfwWindowShouldClose(window))
     {
