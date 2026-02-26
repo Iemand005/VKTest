35924c2df6275458e320521c25ce7fd617a65345
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-02-26 15:26:38 +0100

    swapchain

--- main.cpp
+++ main.cpp
@@ -314,7 +314,14 @@ void createSwapChain() {
     if (swapChainSupport.capabilities.maxImageCount > 0 && imageCount > swapChainSupport.capabilities.maxImageCount) {
     imageCount = swapChainSupport.capabilities.maxImageCount;//#
 
-    VkSwapchainCreateInfoKHR createInfo{};
+
+
+
+    QueueFamilyIndices indices = findQueueFamilies(physicalDevice);
+uint32_t queueFamilyIndices[] = {indices.graphicsFamily.value(), indices.presentFamily.value()};
+
+
+VkSwapchainCreateInfoKHR createInfo{};
 createInfo.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
 createInfo.surface = surface;
 
@@ -324,10 +331,6 @@ createInfo.imageColorSpace = surfaceFormat.colorSpace;
 createInfo.imageExtent = extent;
 createInfo.imageArrayLayers = 1;
 createInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
-
-
-    QueueFamilyIndices indices = findQueueFamilies(physicalDevice);
-uint32_t queueFamilyIndices[] = {indices.graphicsFamily.value(), indices.presentFamily.value()};
 
 if (indices.graphicsFamily != indices.presentFamily) {
     createInfo.imageSharingMode = VK_SHARING_MODE_CONCURRENT;
@@ -339,23 +342,30 @@ if (indices.graphicsFamily != indices.presentFamily) {
     createInfo.pQueueFamilyIndices = nullptr; // Optional
 }
 
+createInfo.preTransform = swapChainSupport.capabilities.currentTransform;
+createInfo.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
+
+createInfo.presentMode = presentMode;
+createInfo.clipped = VK_TRUE;
+
+createInfo.oldSwapchain = VK_NULL_HANDLE;
+
+if (vkCreateSwapchainKHR(device, &createInfo, nullptr, &swapChain) != VK_SUCCESS) {
+    throw std::runtime_error("failed to create swap chain!");
+}
+
 
 }
 
 
-// VkSwapchainCreateInfoKHR createInfo{};
-// createInfo.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
-// createInfo.surface = surface;
 
-// createInfo.minImageCount = imageCount;
-// createInfo.imageFormat = surfaceFormat.format;
-// createInfo.imageColorSpace = surfaceFormat.colorSpace;
-// createInfo.imageExtent = extent;
-// createInfo.imageArrayLayers = 1;
-// createInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+
+
 }
+VkSwapchainKHR swapChain;
 GLFWwindow* window;
   VkPhysicalDevice physicalDevice;
+  VkDevice device;
 
 int main() {
   std::cout << "hi";
@@ -373,8 +383,7 @@ int main() {
   glm::mat4 matrix;
   glm::vec4 vec;
   auto test = matrix * vec;
-
-  VkDevice device;
+  
   VkInstance instance = initVulkan(&device, &physicalDevice);
 
 
@@ -430,7 +439,7 @@ vkGetDeviceQueue(device, indices.presentFamily.value(), 0, &presentQueue);
   vkDestroySurfaceKHR(instance, surface, nullptr);
     // vkDestroyInstance(instance, nullptr);
 
-  vkDestroyInstance(instance, nullptr);
+  vkDestroyInstance(instance, nullptr);    vkDestroySwapchainKHR(device, swapChain, nullptr);
 
   glfwDestroyWindow(window);
 
