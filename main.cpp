6b496150028228dd93bec253306c778b790961a3
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-02-26 16:38:04 +0100

    pipeline

--- main.cpp
+++ main.cpp
@@ -154,28 +154,15 @@ rasterizer.cullMode = VK_CULL_MODE_BACK_BIT;
 rasterizer.frontFace = VK_FRONT_FACE_CLOCKWISE;
 
 rasterizer.depthBiasEnable = VK_FALSE;
-rasterizer.depthBiasConstantFactor = 0.0f; // Optional
-rasterizer.depthBiasClamp = 0.0f; // Optional
-rasterizer.depthBiasSlopeFactor = 0.0f; // Optional
 
 VkPipelineMultisampleStateCreateInfo multisampling{};
 multisampling.sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
 multisampling.sampleShadingEnable = VK_FALSE;
 multisampling.rasterizationSamples = VK_SAMPLE_COUNT_1_BIT;
-multisampling.minSampleShading = 1.0f; // Optional
-multisampling.pSampleMask = nullptr; // Optional
-multisampling.alphaToCoverageEnable = VK_FALSE; // Optional
-multisampling.alphaToOneEnable = VK_FALSE; // Optional
 
 VkPipelineColorBlendAttachmentState colorBlendAttachment{};
 colorBlendAttachment.colorWriteMask = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT;
 colorBlendAttachment.blendEnable = VK_FALSE;
-colorBlendAttachment.srcColorBlendFactor = VK_BLEND_FACTOR_ONE; // Optional
-colorBlendAttachment.dstColorBlendFactor = VK_BLEND_FACTOR_ZERO; // Optional
-colorBlendAttachment.colorBlendOp = VK_BLEND_OP_ADD; // Optional
-colorBlendAttachment.srcAlphaBlendFactor = VK_BLEND_FACTOR_ONE; // Optional
-colorBlendAttachment.dstAlphaBlendFactor = VK_BLEND_FACTOR_ZERO; // Optional
-colorBlendAttachment.alphaBlendOp = VK_BLEND_OP_ADD; // Optional
 
 colorBlendAttachment.blendEnable = VK_TRUE;
 colorBlendAttachment.srcColorBlendFactor = VK_BLEND_FACTOR_SRC_ALPHA;
@@ -188,13 +175,18 @@ colorBlendAttachment.alphaBlendOp = VK_BLEND_OP_ADD;
 VkPipelineColorBlendStateCreateInfo colorBlending{};
 colorBlending.sType = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO;
 colorBlending.logicOpEnable = VK_FALSE;
-colorBlending.logicOp = VK_LOGIC_OP_COPY; // Optional
 colorBlending.attachmentCount = 1;
 colorBlending.pAttachments = &colorBlendAttachment;
-colorBlending.blendConstants[0] = 0.0f; // Optional
-colorBlending.blendConstants[1] = 0.0f; // Optional
-colorBlending.blendConstants[2] = 0.0f; // Optional
-colorBlending.blendConstants[3] = 0.0f; // Optional
+
+
+VkPipelineLayout pipelineLayout;
+
+VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
+pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+
+if (vkCreatePipelineLayout(device, &pipelineLayoutInfo, nullptr, &pipelineLayout) != VK_SUCCESS) {
+    throw std::runtime_error("failed to create pipeline layout!");
+}
 }
 
 VkShaderModule createShaderModule(const std::vector<char>& code) {
