8957f748680ba85758a7ee210cb8b88d37e0d378
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-02-26 12:36:28 +0100

    Add htingies

--- main.cpp
+++ main.cpp
@@ -2,13 +2,17 @@
 #include <iostream>
 
 #define GLFW_INCLUDE_VULKAN
+#define VK_USE_PLATFORM_WIN32_KHR
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
+#define GLFW_EXPOSE_NATIVE_WIN32
+#include <GLFW/glfw3native.h>
 
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #include <glm/vec4.hpp>
 #include <glm/mat4x4.hpp>
+
 
 
 #pragma comment(lib, "vulkan-1.lib")
@@ -43,6 +47,67 @@ VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
 if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
     throw std::runtime_error("failed to create instance!");
 }
+
+VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+
+uint32_t deviceCount = 0;
+vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+
+if (deviceCount == 0) {
+    throw std::runtime_error("failed to find GPUs with Vulkan support!");
+}
+
+std::vector<VkPhysicalDevice> devices(deviceCount);
+vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+
+VkPhysicalDevice e = devices.at(0);
+
+uint32_t queueFamilyCount = 0;
+vkGetPhysicalDeviceQueueFamilyProperties(e, &queueFamilyCount, nullptr);
+
+std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+vkGetPhysicalDeviceQueueFamilyProperties(e, &queueFamilyCount, queueFamilies.data());
+
+int i = 0;
+for (const auto& queueFamily : queueFamilies) {
+    if (queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+        // indices.graphicsFamily = i;
+        break;
+    }
+
+    i++;
+}
+
+VkDeviceQueueCreateInfo queueCreateInfo{};
+queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+queueCreateInfo.queueFamilyIndex = i;
+queueCreateInfo.queueCount = 1;
+
+float queuePriority = 1.0f;
+queueCreateInfo.pQueuePriorities = &queuePriority;
+
+VkPhysicalDeviceFeatures deviceFeatures{};
+
+VkDeviceCreateInfo deviceCreateInfo{};
+deviceCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+
+deviceCreateInfo.enabledExtensionCount = 0;
+
+  deviceCreateInfo.enabledLayerCount = 0;
+
+  VkDevice device;
+
+  if (vkCreateDevice(physicalDevice, &deviceCreateInfo, nullptr, &device) != VK_SUCCESS) {
+    throw std::runtime_error("failed to create logical device!");
+}
+
+VkQueue graphicsQueue;
+
+vkGetDeviceQueue(device, queueCreateInfo.queueFamilyIndex, 0, &graphicsQueue);
+
+
+VkSurfaceKHR surface;
+
 
 return instance;
 }
