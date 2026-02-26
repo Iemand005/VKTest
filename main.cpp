b5001ae2f7381ad44a06cef86aa983cf835c4df8
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-02-26 11:47:59 +0100

    Fix build and add mmore base

--- main.cpp
+++ main.cpp
@@ -10,8 +10,32 @@
 #include <glm/vec4.hpp>
 #include <glm/mat4x4.hpp>
 
+#pragma comment(lib, "vulkan-1.lib")
+
 int main() {
   std::cout << "hi";
 
   glfwInit();
+
+  glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+  GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+
+  uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+    std::cout << extensionCount << " extensions supported\n";
+
+    glm::mat4 matrix;
+    glm::vec4 vec;
+    auto test = matrix * vec;
+
+    while(!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+
+    return 0;
 }
