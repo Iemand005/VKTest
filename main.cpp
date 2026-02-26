194b20c5d4f53fba3940d769138cdeb5954755a1
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-02-26 11:39:27 +0100

    init

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 
 #include <iostream>
 
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+
 int main() {
   std::cout << "hi";
+
+  glfwInit();
 }
